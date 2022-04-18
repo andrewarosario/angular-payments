3b958477bcf5fe643a9b31ca65b6c9079ef99358
andrewarosario <andrew.arosario@gmail.com>
2022-04-18 17:57:04 -0300

    feat: add CoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
 @NgModule({
-  declarations: [	
-    AppComponent,
-   ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CoreModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
