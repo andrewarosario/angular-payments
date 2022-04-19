122c0960ef569c326c6870fc29b69471cacd97b8
andrewarosario <andrew.arosario@gmail.com>
2022-04-19 10:37:44 -0300

    feat(login): add text content

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,18 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, waitForAsync } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe(AppComponent.name, () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
 });
--- src/app/pages/login-page/login-page.component.spec.ts
+++ src/app/pages/login-page/login-page.component.spec.ts
@@ -1,25 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { LogoComponentModule } from "src/app/components/logo/logo.component.module";
 
-import { LoginPageComponent } from './login-page.component';
+import { LoginPageComponent } from "./login-page.component";
 
-describe('LoginPageComponent', () => {
+describe(LoginPageComponent.name, () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginPageComponent ]
-    })
-    .compileComponents();
+      imports: [LogoComponentModule],
+      declarations: [LoginPageComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginPageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("should render title and welcome text", () => {
+    const titleElement = fixture.nativeElement.querySelector("h1");
+    const welcomeElement = fixture.nativeElement.querySelector("h2");
+    expect(titleElement.textContent.trim()).toBe("PayFriends");
+    expect(welcomeElement.textContent.trim()).toBe("Bem vindo de volta");
+  });
+
+  it("should render image with correct path and alt", () => {
+    fixture.detectChanges();
+    const imageElement = fixture.nativeElement.querySelector("img");
+    expect(imageElement.getAttribute("src")).toBe(component.imagePath);
+    expect(imageElement.getAttribute("alt")).toBe("Login image");
   });
 });
