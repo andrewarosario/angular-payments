3389003b0afed2a03ee34e0a583a622abd2f92b0
andrewarosario <andrew.arosario@gmail.com>
2022-04-19 17:40:18 -0300

    feat(login): navigate to payments

--- src/app/pages/login-page/login-page.component.spec.ts
+++ src/app/pages/login-page/login-page.component.spec.ts
@@ -1,19 +1,39 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterTestingModule } from "@angular/router/testing";
 import { LogoComponentModule } from "src/app/components/logo/logo.component.module";
+import { AuthService } from "src/app/services/auth/auth.service";
 import { LoginFormModule } from "./login-form/login-form.module";
+import { of } from "rxjs";
 
 import { LoginPageComponent } from "./login-page.component";
+import { mockUser } from "src/app/mocks/user.mock";
+import { By } from "@angular/platform-browser";
+import { LoginFormComponent } from "./login-form/login-form.component";
+import { Router } from "@angular/router";
 
 describe(LoginPageComponent.name, () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
+  let router: Router;
 
   beforeEach(async () => {
+    authServiceSpy = jasmine.createSpyObj<AuthService>("AuthService", {
+      auth: of(mockUser),
+    });
     await TestBed.configureTestingModule({
-      imports: [BrowserAnimationsModule, LogoComponentModule, LoginFormModule],
+      imports: [
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        LogoComponentModule,
+        LoginFormModule,
+      ],
       declarations: [LoginPageComponent],
+      providers: [{ provide: AuthService, useValue: authServiceSpy }],
     }).compileComponents();
+
+    router = TestBed.inject(Router);
   });
 
   beforeEach(() => {
@@ -33,5 +53,14 @@ describe(LoginPageComponent.name, () => {
     const imageElement = fixture.nativeElement.querySelector("img");
     expect(imageElement.getAttribute("src")).toBe(component.imagePath);
     expect(imageElement.getAttribute("alt")).toBe("Login image");
+  });
+
+  it("should navigate to payments page when authenticating", () => {
+    spyOn(router, "navigateByUrl");
+    const loginFormComponent: LoginFormComponent = fixture.debugElement.query(
+      By.directive(LoginFormComponent)
+    ).componentInstance;
+    loginFormComponent.submitForm.emit();
+    expect(router.navigateByUrl).toHaveBeenCalledWith("payments");
   });
 });
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,21 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { LoginForm } from "src/app/models/login-form";
+import { AuthService } from "src/app/services/auth/auth.service";
 
 @Component({
-  selector: "app-login-page",
   templateUrl: "./login-page.component.html",
   styleUrls: ["./login-page.component.scss"],
 })
 export class LoginPageComponent {
   imagePath = "assets/images/login.svg";
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  submitForm(userData: LoginForm): void {
+    this.authService.auth(userData).subscribe(
+      () => this.router.navigateByUrl("payments"),
+      (err) => console.log({ err })
+    );
+  }
 }
