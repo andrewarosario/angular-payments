23291c0859528c207acf20820f61b95f140809d3
andrewarosario <andrew.arosario@gmail.com>
2022-04-19 08:25:02 -0300

    chore: add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule],
+  imports: [BrowserModule, CoreModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
