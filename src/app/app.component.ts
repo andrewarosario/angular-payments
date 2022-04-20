250b091eb75a3bc6681bff5ac91beded8d1eff2a
andrewarosario <andrew.arosario@gmail.com>
2022-04-20 00:17:20 -0300

    feat(global): add material theme style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements OnInit{
-  title: string;
-
-  ngOnInit() {
-   this.title = 'Desafio Picpay Front-end';
-  }
-}
+export class AppComponent {}
